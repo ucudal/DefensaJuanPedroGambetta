636c574636e51f9d25836307c396bd53be8985c2
JuanGambetta <88690180+JuanGambetta@users.noreply.github.com>
2021-12-01 21:01:25 -0300

    ej5Finalizado

--- p5/src/Library/Residue.cs
+++ p5/src/Library/Residue.cs
@@ -19,7 +19,7 @@ namespace Ucu.Poo.Defense
         /// <param name="obj">El objeto a comparar.</param>
         /// <returns><c>True</c> si este objeto es igual al que se recibe como parámetro; <c>False</c> en caso
         /// contrario.</returns>
-        public bool Equals(Residue obj)
+        public override bool Equals(object obj)
         {
             // Si el otro objeto es null no puede ser igual a este objeto que no es null; retornar False
             if (obj == null)
@@ -38,7 +38,6 @@ namespace Ucu.Poo.Defense
                 return this.Type.Equals(((Residue)obj).Type) &&
                     this.Name.Equals(((Residue)obj).Name);
             }
-            //el problema es con el tipo object, cuando comparo si object es igual a un subtipo de object
         }
     }
 }
